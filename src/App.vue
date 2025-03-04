748d7a9f1e846cd77d4a5773341dc3d7a93e832a
Aysha <ayshatandroid31@gmail.com>
2025-03-04 23:52:23 +0300

    qqq

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <h3>{{ info }}</h3>
-  <p>{{ someInfo }}</p>
 
-  <button type="button" @click="this.info = 'Some new '">Отправить</button>
 </template>
 
 <script>
 export default {
   data(){
     return{
-      info: 'Title!',
-      someInfo: 'Anons some message'
+  
     }
+  },
+  methods:{
+
   }
 }
 </script>
 
 <style>
-  h3{
-    font-weight: lighter;
-  }
 
-  p{
-    color: red;
-  }
 </style>
