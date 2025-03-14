5455abf084e1197f7e6c6842e4f6b579ce9bae50
Aysha <ayshatandroid31@gmail.com>
2025-03-14 10:26:02 +0300

    toDoList

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <template>
-
+  <from @submit.prevent="AddToCard">
+    <label>Текст</label>
+    <input  
+      type="text" 
+      v-model="msg"
+      placeholder="Введите текст"/>
+    <input 
+      type="submit"
+      @click="AddToCard"
+      >
+  </from>
+ 
+  <ul>
+    <li v-if="items.length === 0">Тут нечего нет</li>
+    <li 
+      v-for="item in items" 
+      :key="item.id">
+        {{ item.msg}}
+      <button @click="deleteApp(item.id)">Удалить</button>  
+      </li>
+  </ul>
 </template>
-
-<script>
-export default {
-  data(){
-    return{
-  
-    }
-  },
-  methods:{
-
+ 
+<script setup>
+import { ref } from 'vue'
+ 
+const msg = ref('');
+const items = ref([])
+ 
+const AddToCard = () => {
+  if(msg.value.trim() === '') {
+    alert("Введите текст")
+    return
   }
+ 
+  items.value.push({
+    id: Date.now(),
+    msg: msg.value.trim()
+  })
+ 
+  msg.value = ''
+}
+ 
+const deleteApp = (id) => {
+  items.value = items.value.filter(item => item.id !== id)
 }
 </script>
-
-<style>
-
-  p{
-    color: red;
-    font-size: 30px;
-  }
+ 
+<style scoped>
+ 
+ul > li {
+ 
+  list-style: none;
+}
 </style>
